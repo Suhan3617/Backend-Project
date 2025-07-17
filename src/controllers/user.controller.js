0976278b787445c3827453fb886384d28be8ba0a
Suhan3617 <24je0860@iitism.ac.in>
2025-07-17 16:36:22 +0530

    user logout and logout middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -143,7 +143,7 @@ const loginUser = asyncHandler(async (req, res) => {
 
   const {accessToken , refreshToken } = await generateAccessAndRefreshTokens(user._id)
 
-  const loggedInUser = await User.findById(user._id).select("-password -refreshToken ")
+  const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
 
   const options = {
     httpOnly : true,
@@ -166,4 +166,30 @@ const loginUser = asyncHandler(async (req, res) => {
 
 });
 
-export { registerUser, loginUser };
+
+const logoutUser = asyncHandler(async (req,res)=>{
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set : {
+        refreshToken : undefined
+      }
+    },
+    {
+      new:true
+    }
+  )
+
+  const options = {
+    httpOnly : true,
+    secure : true,
+  }
+
+  return res
+  .status(200)
+  .clearCookie("accessToken", options)
+  .clearCookie("refreshToken", options)
+  .json(new ApiResponse(200,{},"User Logout Successfully"))
+})
+
+export { registerUser, loginUser , logoutUser};
