a684c8a57ca616ceac98b06212f425092308a7e8
Suhan3617 <24je0860@iitism.ac.in>
2025-07-18 19:33:45 +0530

    minor updates

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -252,4 +252,10 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
   }
 });
 
-export { registerUser, loginUser, logoutUser ,refreshAccessToken};
+export{
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  generateAccessAndRefreshTokens
+}
